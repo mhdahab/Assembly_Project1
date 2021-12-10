3dcbca6aae3ffe7501b634bf516edb87fca980c7
mhdahab <67865770+mhdahab@users.noreply.github.com>
2021-12-10 14:43:52 +0200

    Add files via upload

--- DirectMapping/main.cpp
+++ DirectMapping/main.cpp
@@ -6,7 +6,16 @@
 using namespace std;
 //idnex /valid/tag
 
-map<string, map<string, string>> cache;
+map<string, map<string, string>> cache_data;
+map<string, map<string, string>> cache_inst;
+int numberOfAccess_data = 0;
+int numberOfAccess_inst = 0;
+double missRatio_data = 0;
+double hitRatio_data = 0;
+double missRatio_inst = 0;
+double hitRatio_inst = 0;
+double AMAT_data = 0;
+double AMAT_inst = 0;
 
 string tobinary(int n , int indexSize) {
 	string result = "";
@@ -21,36 +30,87 @@ string tobinary(int n , int indexSize) {
 }
 void PrintMap()
 {
+	cout << "Total Number of Accesses : " << numberOfAccess_data + numberOfAccess_inst << endl;
+	cout << endl;
+	cout << "Data Cache" << endl;
+	cout << endl;
+	cout << "Number of Access for Data Cache : " << numberOfAccess_data << endl;
+	cout << "Hit Ratio : " << hitRatio_data << endl;
+	cout << "Miss Ratio : " << missRatio_data << endl;
+	cout << "Average Memory Access Time : " << AMAT_data << endl;
 	cout << "Index		Valid		  Tag" << endl;
-	for (auto itr = cache.begin(); itr != cache.end(); itr++) {
+
+	for (auto itr = cache_data.begin(); itr != cache_data.end(); itr++) {
 		for (auto ptr = itr->second.begin(); ptr != itr->second.end(); ptr++) {
 			cout << itr->first << "		  " << ptr->first << "		" << ptr->second << endl;
 		}
 	}
 
+
+	cout << endl;
+	cout << "Data Instruction" << endl;
+	cout << endl;
+	cout << "Number of Access for Instruction Cache : " << numberOfAccess_inst << endl;
+	cout << "Hit Ratio : " << hitRatio_inst << endl;
+	cout << "Miss Ratio : " << missRatio_inst << endl;
+	cout << "Average Memory Access Time : " << AMAT_inst << endl;
+	cout << "Index		Valid		  Tag" << endl;
+
+	for (auto itr = cache_inst.begin(); itr != cache_inst.end(); itr++) {
+		for (auto ptr = itr->second.begin(); ptr != itr->second.end(); ptr++) {
+			cout << itr->first << "		  " << ptr->first << "		" << ptr->second << endl;
+		}
+	}
+
+	cout << endl;
 	system("pause");
 }
 int main()
 {
-	int S, L, cycleNumbers;
+	int S_data, L_data, cycleNumbers_data;
+	int S_inst, L_inst, cycleNumbers_inst;
 
-	cout << "Enter Cache Size : " << endl;
-	cin >> S; //1024
-	cout << "Enter Cache Line Size :" << endl;
-	cin >> L; //128
-	cout << "Enter Number of Cycles :" << endl;
-	cin >> cycleNumbers;
+	cout << "Enter Cache Size for Data Cache : " << endl;
+	cin >> S_data; //1024
+	cout << "Enter Cache Line Size for Data Cache :" << endl;
+	cin >> L_data; //128
+	cout << "Enter Number of Cycles for Data Cache :" << endl;
+	cin >> cycleNumbers_data;
 
-	int dataSize = log2(L); 
-	int C = S / L;
-	int indexSize = log2(C); 
-	int tagSize = 32 - (dataSize + indexSize);
+	cout << "Enter Cache Size for Instruction Cache : " << endl;
+	cin >> S_inst; //1024
+	cout << "Enter Cache Line Size for Instruction Cache :" << endl;
+	cin >> L_inst; //128
+	cout << "Enter Number of Cycles for Instruction Cache :" << endl;
+	cin >> cycleNumbers_inst;
 
-	string j = "0";
-	for (int i = 0; i < C; i++)
+
+
+
+	int dataSize = log2(L_data); 
+	int C_data = S_data / L_data;
+	int indexSize_data = log2(C_data);
+	int tagSize_data = 32 - (dataSize + indexSize_data);
+
+
+
+	int instSize = log2(L_inst);
+	int C_inst = S_inst / L_inst;
+	int indexSize_inst = log2(C_inst);
+	int tagSize_inst = 32 - (instSize + indexSize_inst);
+
+
+	for (int i = 0; i < C_data; i++)
 	{
-		cache[tobinary(i,indexSize)][j] = " ";
+		cache_data[tobinary(i,indexSize_data)]["0"] = " ";
 	}
+
+	for (int i = 0; i < C_inst; i++)
+	{
+		cache_inst[tobinary(i, indexSize_inst)]["0"] = " ";
+	}
+
+
 
 	string inputfile;
 
@@ -62,49 +122,91 @@ int main()
 
 	string address;
 	int data = 32 - dataSize;
-	int numberOfAccess = 1;
-	int hit = 0;
-	int miss = 0;
+	int inst = 32 - instSize;
+	int miss_data = 0;
+	int hit_data = 0;
+	int miss_inst = 0;
+	int hit_inst = 0;
 	while (getline(infile, address))
 	{
-		cout << "Number of Access : " << numberOfAccess << endl;
-		string value = address.substr(0, data);
-		string tag = value.substr(0, tagSize);
-		string index = value.substr(tagSize);
-
-		auto itr = cache.find(index);
-		auto ptr = itr->second.begin();
-		if (ptr->first == "0")
+		if (address[0] == 'D')
 		{
-			miss++;
-			cache[index].erase("0");
-			cache[index]["1"] = tag;
-		}
-		else if (ptr->first == "1")
-		{
-			if (ptr->second == tag)
+			numberOfAccess_data++;
+			address.replace(0, 2, "");
+			string value = address.substr(0, data);
+			string tag = value.substr(0, tagSize_data);
+			string index = value.substr(tagSize_data);
+
+			auto itr = cache_data.find(index);
+			auto ptr = itr->second.begin();
+			if (ptr->first == "0")
 			{
-				hit++;
+				miss_data++;
+				cache_data[index].erase("0");
+				cache_data[index]["1"] = tag;
 			}
-			else
+			else if (ptr->first == "1")
 			{
-				miss++;
-				cache[index]["1"] = tag;
+				if (ptr->second == tag)
+				{
+					hit_data++;
+				}
+				else
+				{
+					miss_data++;
+					cache_data[index]["1"] = tag;
+
+				}
+
 
 			}
 
+			missRatio_data = double(miss_data) / double(numberOfAccess_data);
+			hitRatio_data = double(hit_data) / double(numberOfAccess_data);
+			AMAT_data = double(cycleNumbers_data) + missRatio_data * 100.00;
+			PrintMap();
+			
 
 		}
+		else if(address[0]=='I')
+		{ 
+			numberOfAccess_inst++;
+			address.replace(0, 2, "");
+			string value = address.substr(0, inst);
+			string tag = value.substr(0, tagSize_inst);
+			string index = value.substr(tagSize_inst);
 
-		double missRatio = double(miss) / double(numberOfAccess);
-		double hitRatio = double(hit) / double(numberOfAccess);
-		cout << "Hit Ratio : " << hitRatio << endl;
-		cout << "Miss Ratio : " << missRatio << endl;
-		double AMAT = double(cycleNumbers) + missRatio * 100.00;
-		cout << "Average Memory Access Time : " << AMAT << endl;
-		PrintMap();
-		numberOfAccess++;
-		cout << endl;
+			auto itr = cache_inst.find(index);
+			auto ptr = itr->second.begin();
+			if (ptr->first == "0")
+			{
+				miss_inst++;
+				cache_inst[index].erase("0");
+				cache_inst[index]["1"] = tag;
+			}
+			else if (ptr->first == "1")
+			{
+				if (ptr->second == tag)
+				{
+					hit_inst++;
+				}
+				else
+				{
+					miss_inst++;
+					cache_inst[index]["1"] = tag;
+
+				}
+
+
+			}
+
+			missRatio_inst = double(miss_inst) / double(numberOfAccess_inst);
+			hitRatio_inst = double(hit_inst) / double(numberOfAccess_inst);
+			AMAT_inst = double(cycleNumbers_inst) + missRatio_inst * 100.00;
+			PrintMap();
+			
+		}
+		
 
 	}
 
